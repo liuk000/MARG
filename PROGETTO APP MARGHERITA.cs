e9f43636b55bc2c0cc51474a1e16ca3d2f3c9c02
lucabertini <lucabertiniluca@gmail.com>
2026-03-18 12:13:27 +0100

    minor update

---
+++ PROGETTO APP MARGHERITA.cs
@@ -0,0 +1,21 @@
+PROGETTO APP MARGHERITA
+
+1) App funzionante in locale
+2) Pagare 99 euro per Apple Developer Program
+3) Pubblicare l'app su App Store
+4) Promuovere l'app per aumentare le installazioni
+
+
+
+CARTELLA PROGETTO APP MARGHERITA: /Users/lucabertini/margherita
+XCODE:
+
+si apre in due modi: 
+
+1) "XCODE HERE" nella cartella principale
+2) "/Users/lucabertini/margherita/ios/Runner.xcworkspace" questo
+
+
+Selezionare Account liuk@me.com
+
+AVERE SITO INTERNET PER PUBBLICARE L'APP SU APP STORE
